b8d54bcf76719c04c3065f3de9d23791a6c02b1b
rong3000 <rong.lin3000@gmail.com>
2019-07-10 08:20:43 +1000

    navigation setup.

---
+++ lib/pages/receipts_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ReceiptsPage extends StatefulWidget {
+  @override
+  _ReceiptsPageState createState() => _ReceiptsPageState();
+}
+
+class _ReceiptsPageState extends State<ReceiptsPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Receipts'),
+      ),
+    );
+  }
+}
