4056933d7806d63ad37d4f3416cf7e52f3dd390a
rong3000 <rong.lin3000@gmail.com>
2019-07-25 09:35:16 +1000

    layouttrying2

--- lib/pages/reports_page.dart
+++ lib/pages/reports_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class ReportsPage extends StatefulWidget {
@@ -7,155 +6,146 @@ class ReportsPage extends StatefulWidget {
 }
 
 class _ReportsPageState extends State<ReportsPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Flexible(
-            flex: 12,
-            fit: FlexFit.tight,
-            child: Column(
-              children: <Widget>[
-                Flexible(
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: FractionallySizedBox(
-                          heightFactor: 1,
-                          child: Card(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                const ListTile(
-
-                                  title: Text('Add Your (First) Receipt'),
-                                ),
-                              ],
-                            ),
+      body: OrientationBuilder(builder: (context, orientation){
+        return
+          Column(
+            children: <Widget>[
+              Flexible(
+                fit: FlexFit.tight,
+                child: Wrap(
+                  children: <Widget>[
+                    FractionallySizedBox(
+                      child: FractionallySizedBox(
+                        widthFactor: orientation == Orientation.portrait ? 0.5: 0.25,
+                        child: Card(
+                          child: ListTile(
+                            title: Text('Add Your (First) Receipt'),
                           ),
                         ),
                       ),
-                      Expanded(
-                        child: FractionallySizedBox(
-                          heightFactor: 1,
-                          child: Card(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                const ListTile(
-
-                                  title: Text(
-                                      'Manually Add Your (First) Receipt'),
-                                ),
-                              ],
-                            ),
+                    ),
+                    FractionallySizedBox(
+                      widthFactor: orientation == Orientation.portrait ? 0.5: 0.25,
+                      child: Card(
+                        child: ListTile(
+                          title: Text('Add Your (First) Receipt'),
+                        ),
+                      ),
+                    ),
+                    FractionallySizedBox(
+                      widthFactor: orientation == Orientation.portrait ? 0.5: 0.25,
+                      child: Card(
+                        child: ListTile(
+                          title: Text('Add Your (First) Receipt'),
+                        ),
+                      ),
+                    ),
+                    FractionallySizedBox(
+                      widthFactor: orientation == Orientation.portrait ? 0.5: 0.25,
+                      child: Card(
+                        child: ListTile(
+                          title: Text('Add Your (First) Receipt'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Flexible(
+                  fit: FlexFit.tight,
+                  child: Wrap(
+                    children: <Widget>[
+                      FractionallySizedBox(
+                        widthFactor: orientation == Orientation.portrait ? 1: 0.33,
+                        child: Card(
+                          child: ListTile(
+                            title: Text('Add Your (First) Receipt'),
+                          ),
+                        ),
+                      ),
+                      FractionallySizedBox(
+                        widthFactor: orientation == Orientation.portrait ? 1: 0.33,
+                        child: Card(
+                          child: ListTile(
+                            title: Text('Add Your (First) Receipt'),
+                          ),
+                        ),
+                      ),
+                      FractionallySizedBox(
+                        widthFactor: orientation == Orientation.portrait ? 1: 0.33,
+                        child: Card(
+                          child: ListTile(
+                            title: Text('Add Your (First) Receipt'),
                           ),
                         ),
                       ),
                     ],
-                  ),
-                ),
-                Flexible(
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: FractionallySizedBox(
-                          heightFactor: 1,
-                          child: Card(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                const ListTile(
+                  )
+              ),
 
-                                  title: Text('View Imported Receipts'),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: FractionallySizedBox(
-                          heightFactor: 1,
-                          child: Card(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                const ListTile(
+//          Flexible(
+//            flex: 10,
+//            fit: FlexFit.tight,
+//            child: Column(
+//              children: <Widget>[
+//                Flexible(
+//                  fit: FlexFit.tight,
+//                  child: Card(
+//                    child: Column(
+//                      mainAxisSize: MainAxisSize.min,
+//                      children: <Widget>[
+//                        const ListTile(
+//                          leading: Icon(Icons.album),
+//                          title: Text('Intelligent Receipt'),
+//                          subtitle: Text(
+//                              'Invite your friends to join IR then receive more free automatically scans'),
+//                        ),
+//                      ],
+//                    ),
+//                  ),
+//                ),
+//                Flexible(
+//                  fit: FlexFit.tight,
+//                  child: Card(
+//                    child: Column(
+//                      mainAxisSize: MainAxisSize.min,
+//                      children: <Widget>[
+//                        const ListTile(
+//                          leading: Icon(Icons.album),
+//                          title: Text('Intelligent Receipt'),
+//                          subtitle:
+//                          Text('Get unlimited automatically scans'),
+//                        ),
+//                      ],
+//                    ),
+//                  ),
+//                ),
+//                Flexible(
+//                  fit: FlexFit.tight,
+//                  child: Card(
+//                    child: Column(
+//                      mainAxisSize: MainAxisSize.min,
+//                      children: <Widget>[
+//                        const ListTile(
+//                          leading: Icon(Icons.album),
+//                          title: Text('Intelligent Receipt'),
+//                          subtitle: Text(
+//                              'We have sent you an email, please click confirm'),
+//                        ),
+//                      ],
+//                    ),
+//                  ),
+//                ),
+//              ],
+//            ),
+//          ),
+            ],
+          );
+      }),
 
-                                  title: Text('View Reports'),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Flexible(
-            flex: 10,
-            fit: FlexFit.tight,
-            child: Column(
-              children: <Widget>[
-                Flexible(
-                  fit: FlexFit.tight,
-                  child: Card(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        const ListTile(
-                          leading: Icon(Icons.album),
-                          title: Text('Intelligent Receipt'),
-                          subtitle: Text(
-                              'Invite your friends to join IR then receive more free automatically scans'),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Flexible(
-                  fit: FlexFit.tight,
-                  child: Card(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        const ListTile(
-                          leading: Icon(Icons.album),
-                          title: Text('Intelligent Receipt'),
-                          subtitle:
-                          Text('Get unlimited automatically scans'),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Flexible(
-                  fit: FlexFit.tight,
-                  child: Card(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        const ListTile(
-                          leading: Icon(Icons.album),
-                          title: Text('Intelligent Receipt'),
-                          subtitle: Text(
-                              'We have sent you an email, please click confirm'),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
