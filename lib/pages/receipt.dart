672a31911777c464547482e4498c9ad358d9c2c8
Rong Lin <rong.lin3000@gmail.com>
2019-08-02 21:27:48 +1000

    Receipt Page UI done.

--- lib/pages/receipt.dart
+++ lib/pages/receipt.dart
@@ -4,16 +4,17 @@ class Receipt {
   String Amount;
   String Company;
   String Category;
+  String Actions;
 
-  Receipt({this.id, this.Date, this.Amount, this.Company, this.Category});
+  Receipt({this.id, this.Date, this.Amount, this.Company, this.Category, this.Actions});
 
   static List<Receipt> getReceipts() {
     return <Receipt>[
-      Receipt(id: 1, Date: "19/06/2019", Company: "Coles", Category: "Banana", Amount: "100.00"),
-      Receipt(id: 2, Date: "17/05/2019", Company: "Coles", Category: "Banana", Amount: "57.80"),
-      Receipt(id: 3, Date: "22/03/2019", Company: "Coles", Category: "Banana", Amount: "666.66"),
-      Receipt(id: 4, Date: "15/03/2019", Company: "Coles", Category: "Banana", Amount: "49.95"),
-      Receipt(id: 5, Date: "12/03/2019", Company: "Coles", Category: "Banana", Amount: "87.75"),
+      Receipt(id: 1, Date: "19/06/2019", Company: "Coles", Category: "Banana", Actions: "View & Modify", Amount: "100.00"),
+      Receipt(id: 2, Date: "17/05/2019", Company: "Coles", Category: "Banana", Actions: "View & Modify", Amount: "57.80"),
+      Receipt(id: 3, Date: "22/03/2019", Company: "Coles", Category: "Banana", Actions: "View & Modify", Amount: "666.66"),
+      Receipt(id: 4, Date: "15/03/2019", Company: "Coles", Category: "Banana", Actions: "View & Modify", Amount: "49.95"),
+      Receipt(id: 5, Date: "12/03/2019", Company: "Coles", Category: "Banana", Actions: "View & Modify", Amount: "87.75"),
     ];
   }
 }
--- lib/pages/receipts_page.dart
+++ lib/pages/receipts_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intelligent_receipt/main_screen/bloc/bloc.dart';
 import 'package:flutter/rendering.dart';
 
+import 'data_table.dart';
 
 class TabsExample extends StatelessWidget {
   const TabsExample({Key key}) : super(key: key);
@@ -188,93 +189,93 @@ class DessertDataSource extends DataTableSource {
   }
 }
 
-class DataTableDemo extends StatefulWidget {
-  static const String routeName = '/material/data-table';
-
-  @override
-  _DataTableDemoState createState() => _DataTableDemoState();
-}
-
-class _DataTableDemoState extends State<DataTableDemo> {
-  int _rowsPerPage = PaginatedDataTable.defaultRowsPerPage;
-  int _sortColumnIndex;
-  bool _sortAscending = true;
-  final DessertDataSource _dessertsDataSource = DessertDataSource();
-
-  void _sort<T>(Comparable<T> getField(Dessert d), int columnIndex, bool ascending) {
-    _dessertsDataSource._sort<T>(getField, ascending);
-    setState(() {
-      _sortColumnIndex = columnIndex;
-      _sortAscending = ascending;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Scrollbar(
-        child: ListView(
-          padding: const EdgeInsets.all(20.0),
-          children: <Widget>[
-            PaginatedDataTable(
-              header: const Text('Nutrition'),
-              rowsPerPage: _rowsPerPage,
-              availableRowsPerPage: <int>[5, 10],
-              onRowsPerPageChanged: (int value) { setState(() { _rowsPerPage = value; }); },
-              sortColumnIndex: _sortColumnIndex,
-              sortAscending: _sortAscending,
-              onSelectAll: _dessertsDataSource._selectAll,
-              columns: <DataColumn>[
-                DataColumn(
-                  label: const Text('Dessert (100g serving)'),
-                  onSort: (int columnIndex, bool ascending) => _sort<String>((Dessert d) => d.name, columnIndex, ascending),
-                ),
-                DataColumn(
-                  label: const Text('Calories'),
-                  tooltip: 'The total amount of food energy in the given serving size.',
-                  numeric: true,
-                  onSort: (int columnIndex, bool ascending) => _sort<num>((Dessert d) => d.calories, columnIndex, ascending),
-                ),
-                DataColumn(
-                  label: const Text('Fat (g)'),
-                  numeric: true,
-                  onSort: (int columnIndex, bool ascending) => _sort<num>((Dessert d) => d.fat, columnIndex, ascending),
-                ),
-                DataColumn(
-                  label: const Text('Carbs (g)'),
-                  numeric: true,
-                  onSort: (int columnIndex, bool ascending) => _sort<num>((Dessert d) => d.carbs, columnIndex, ascending),
-                ),
-                DataColumn(
-                  label: const Text('Protein (g)'),
-                  numeric: true,
-                  onSort: (int columnIndex, bool ascending) => _sort<num>((Dessert d) => d.protein, columnIndex, ascending),
-                ),
-                DataColumn(
-                  label: const Text('Sodium (mg)'),
-                  numeric: true,
-                  onSort: (int columnIndex, bool ascending) => _sort<num>((Dessert d) => d.sodium, columnIndex, ascending),
-                ),
-                DataColumn(
-                  label: const Text('Calcium (%)'),
-                  tooltip: 'The amount of calcium as a percentage of the recommended daily amount.',
-                  numeric: true,
-                  onSort: (int columnIndex, bool ascending) => _sort<num>((Dessert d) => d.calcium, columnIndex, ascending),
-                ),
-                DataColumn(
-                  label: const Text('Iron (%)'),
-                  numeric: true,
-                  onSort: (int columnIndex, bool ascending) => _sort<num>((Dessert d) => d.iron, columnIndex, ascending),
-                ),
-              ],
-              source: _dessertsDataSource,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//class DataTableDemo extends StatefulWidget {
+//  static const String routeName = '/material/data-table';
+//
+//  @override
+//  _DataTableDemoState createState() => _DataTableDemoState();
+//}
+//
+//class _DataTableDemoState extends State<DataTableDemo> {
+//  int _rowsPerPage = PaginatedDataTable.defaultRowsPerPage;
+//  int _sortColumnIndex;
+//  bool _sortAscending = true;
+//  final DessertDataSource _dessertsDataSource = DessertDataSource();
+//
+//  void _sort<T>(Comparable<T> getField(Dessert d), int columnIndex, bool ascending) {
+//    _dessertsDataSource._sort<T>(getField, ascending);
+//    setState(() {
+//      _sortColumnIndex = columnIndex;
+//      _sortAscending = ascending;
+//    });
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      body: Scrollbar(
+//        child: ListView(
+//          padding: const EdgeInsets.all(20.0),
+//          children: <Widget>[
+//            PaginatedDataTable(
+//              header: const Text('Nutrition'),
+//              rowsPerPage: _rowsPerPage,
+//              availableRowsPerPage: <int>[5, 10],
+//              onRowsPerPageChanged: (int value) { setState(() { _rowsPerPage = value; }); },
+//              sortColumnIndex: _sortColumnIndex,
+//              sortAscending: _sortAscending,
+//              onSelectAll: _dessertsDataSource._selectAll,
+//              columns: <DataColumn>[
+//                DataColumn(
+//                  label: const Text('Dessert (100g serving)'),
+//                  onSort: (int columnIndex, bool ascending) => _sort<String>((Dessert d) => d.name, columnIndex, ascending),
+//                ),
+//                DataColumn(
+//                  label: const Text('Calories'),
+//                  tooltip: 'The total amount of food energy in the given serving size.',
+//                  numeric: true,
+//                  onSort: (int columnIndex, bool ascending) => _sort<num>((Dessert d) => d.calories, columnIndex, ascending),
+//                ),
+//                DataColumn(
+//                  label: const Text('Fat (g)'),
+//                  numeric: true,
+//                  onSort: (int columnIndex, bool ascending) => _sort<num>((Dessert d) => d.fat, columnIndex, ascending),
+//                ),
+//                DataColumn(
+//                  label: const Text('Carbs (g)'),
+//                  numeric: true,
+//                  onSort: (int columnIndex, bool ascending) => _sort<num>((Dessert d) => d.carbs, columnIndex, ascending),
+//                ),
+//                DataColumn(
+//                  label: const Text('Protein (g)'),
+//                  numeric: true,
+//                  onSort: (int columnIndex, bool ascending) => _sort<num>((Dessert d) => d.protein, columnIndex, ascending),
+//                ),
+//                DataColumn(
+//                  label: const Text('Sodium (mg)'),
+//                  numeric: true,
+//                  onSort: (int columnIndex, bool ascending) => _sort<num>((Dessert d) => d.sodium, columnIndex, ascending),
+//                ),
+//                DataColumn(
+//                  label: const Text('Calcium (%)'),
+//                  tooltip: 'The amount of calcium as a percentage of the recommended daily amount.',
+//                  numeric: true,
+//                  onSort: (int columnIndex, bool ascending) => _sort<num>((Dessert d) => d.calcium, columnIndex, ascending),
+//                ),
+//                DataColumn(
+//                  label: const Text('Iron (%)'),
+//                  numeric: true,
+//                  onSort: (int columnIndex, bool ascending) => _sort<num>((Dessert d) => d.iron, columnIndex, ascending),
+//                ),
+//              ],
+//              source: _dessertsDataSource,
+//            ),
+//          ],
+//        ),
+//      ),
+//    );
+//  }
+//}
 
 class ReceiptsPage extends StatefulWidget {
   @override
