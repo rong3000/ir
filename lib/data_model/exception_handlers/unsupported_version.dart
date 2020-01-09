3012b5979d032994c1ad0e5cfdeea550b00cd78f
Song, Bruce <bruce.song@ncr.com>
2020-01-09 16:06:40 +1100

    Force update to call Androd/IOS software upgrade API; and chinese translation

--- lib/data_model/exception_handlers/unsupported_version.dart
+++ lib/data_model/exception_handlers/unsupported_version.dart
@@ -1,12 +1,51 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:intelligent_receipt/translations/global_translations.dart';
+import 'package:in_app_update/in_app_update.dart';
+import 'package:upgrader/upgrader.dart';
+
+void showUpgradeResult(BuildContext context, String message) {
+  showDialog(
+    barrierDismissible: false,
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(allTranslations.text('app.unsupported-version.upgrade-result')),
+        content: Text(allTranslations.text(message)),
+        actions: <Widget>[
+          FlatButton(
+            child: Text(allTranslations.text('words.close')),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          )
+        ],
+      );
+    },
+  );
+}
+
+void onUpgradeButtonPressed(BuildContext context) {
+  if (Platform.isAndroid) {
+    InAppUpdate.startFlexibleUpdate().then((_) {
+      showUpgradeResult(context, allTranslations.text('app.unsupported-version.upgrade-succeeded'));
+    }).catchError((e) => showUpgradeResult(context, allTranslations.text('app.unsupported-version.upgrade-failed-prefix') + e.toString()));
+  } else if (Platform.isIOS) {
+    Upgrader().onUserUpdated(context, true);
+    // how to get upgrade result for IOS
+  } else {
+    showUpgradeResult(context, allTranslations.text('app.unsupported-version.unsupported-platform'));
+  }
+}
 
 void showUnsupportedVersionAlert(BuildContext context) {
   // set up the buttons
   Widget upgradeButton = RaisedButton(
     color: Colors.blue,
     child: Text(allTranslations.text('app.unsupported-version.button')),
-    onPressed:  () {},
+    onPressed:  () {
+      onUpgradeButtonPressed(context);
+    },
   );
 
   // set up the AlertDialog
@@ -49,7 +88,9 @@ class UnsupportedVersion extends StatelessWidget {
               color: Colors.blue,
               textColor: Colors.white,
               child: Text(allTranslations.text('app.unsupported-version.button')),
-              onPressed:  () {},
+              onPressed:  () {
+                onUpgradeButtonPressed(context);
+              },
             )
           ],
         ),
