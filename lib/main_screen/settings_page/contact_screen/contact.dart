42821836187a7c6f33b2072812db5928dd373304
rong3000 <rong.lin3000@gmail.com>
2020-01-20 13:29:19 +1100

    Contact us screen multi language, and codes cleaned up.

--- lib/main_screen/settings_page/contact_screen/contact.dart
+++ lib/main_screen/settings_page/contact_screen/contact.dart
@@ -2,17 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-
-// This file feels very unfinished and no trtanslations have been done - RB
-
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/gestures.dart' show DragStartBehavior;
 import 'package:mailer/mailer.dart';
 import 'package:mailer/smtp_server/hotmail.dart';
-
+import 'package:intelligent_receipt/translations/global_translations.dart';
 import '../../../user_repository.dart';
 
 class ContactUs extends StatefulWidget {
@@ -34,64 +30,6 @@ class PersonData {
   String email = '';
   String password = '';
   String message = '';
-}
-
-class PasswordField extends StatefulWidget {
-  const PasswordField({
-    this.fieldKey,
-    this.hintText,
-    this.labelText,
-    this.helperText,
-    this.onSaved,
-    this.validator,
-    this.onFieldSubmitted,
-  });
-
-  final Key fieldKey;
-  final String hintText;
-  final String labelText;
-  final String helperText;
-  final FormFieldSetter<String> onSaved;
-  final FormFieldValidator<String> validator;
-  final ValueChanged<String> onFieldSubmitted;
-
-  @override
-  _PasswordFieldState createState() => _PasswordFieldState();
-}
-
-class _PasswordFieldState extends State<PasswordField> {
-  bool _obscureText = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      key: widget.fieldKey,
-      obscureText: _obscureText,
-      maxLength: 8,
-      onSaved: widget.onSaved,
-      validator: widget.validator,
-      onFieldSubmitted: widget.onFieldSubmitted,
-      decoration: InputDecoration(
-        border: const UnderlineInputBorder(),
-        filled: true,
-        hintText: widget.hintText,
-        labelText: widget.labelText,
-        helperText: widget.helperText,
-        suffixIcon: GestureDetector(
-          dragStartBehavior: DragStartBehavior.down,
-          onTap: () {
-            setState(() {
-              _obscureText = !_obscureText;
-            });
-          },
-          child: Icon(
-            _obscureText ? Icons.visibility : Icons.visibility_off,
-            semanticLabel: _obscureText ? 'show password' : 'hide password',
-          ),
-        ),
-      ),
-    );
-  }
 }
 
 class ContactUsState extends State<ContactUs> {
@@ -133,18 +71,6 @@ class ContactUsState extends State<ContactUs> {
     }
   }
 
-//  void _handleSubmitted() {
-//    final FormState form = _formKey.currentState;
-//    if (!form.validate()) {
-//      _autovalidate = true; // Start validating on every change.
-//      _showInSnackBar('Please fix the errors in red before submitting.');
-//    } else {
-//      form.save();
-//      mailerSend(message, smtpServer);
-//      _showInSnackBar('Message is sent for ${person.email}', color: Colors.blue, icon: Icons.info);
-//    }
-//  }
-
   String _validateName(String value) {
     _formWasEdited = true;
     if (value.isEmpty)
@@ -164,24 +90,6 @@ class ContactUsState extends State<ContactUs> {
     );
     if (!emailRegExp.hasMatch(value))
       return 'Please enter valid email address.';
-    return null;
-  }
-
-  String _validatePhoneNumber(String value) {
-    _formWasEdited = true;
-    final RegExp phoneExp = RegExp(r'^\(\d\d\d\) \d\d\d\-\d\d\d\d$');
-    if (!phoneExp.hasMatch(value))
-      return '(###) ###-#### - Enter a US phone number.';
-    return null;
-  }
-
-  String _validatePassword(String value) {
-    _formWasEdited = true;
-    final FormFieldState<String> passwordField = _passwordFieldKey.currentState;
-    if (passwordField.value == null || passwordField.value.isEmpty)
-      return 'Please enter a password.';
-    if (passwordField.value != value)
-      return 'The passwords don\'t match';
     return null;
   }
 
@@ -231,7 +139,6 @@ class ContactUsState extends State<ContactUs> {
           ..from = Address(username, 'Your name')
           ..recipients.add('support@superiortech.com.au')
           ..ccRecipients.addAll(['bruce.song.au@gmail.com ', 'rong.lin3000@gmail.com'])
-//      ..ccRecipients.addAll(['destCc1@example.com', 'destCc2@example.com'])
 //      ..bccRecipients.add(Address('bccAddress@example.com'))
           ..subject = '${person.name} Feedback'
           ..text = '${person.message} from ${person.email}';
@@ -245,7 +152,7 @@ class ContactUsState extends State<ContactUs> {
       drawerDragStartBehavior: DragStartBehavior.down,
       key: _scaffoldKey,
       appBar: AppBar(
-        title: const Text('Contact Us'),
+        title: Text(allTranslations.text('app.settings-page.contact-us-menu-item-title')),
       ),
       body: SafeArea(
         top: false,
@@ -264,44 +171,21 @@ class ContactUsState extends State<ContactUs> {
                   const SizedBox(height: 24.0),
                   TextFormField(
                     textCapitalization: TextCapitalization.words,
-                    decoration: const InputDecoration(
+                    decoration: InputDecoration(
                       border: UnderlineInputBorder(),
                       filled: true,
                       icon: Icon(Icons.person),
-                      hintText: 'What do people call you?',
-                      labelText: 'Name',
+                      labelText: allTranslations.text('app.contact-screen.name'),
                     ),
                     onSaved: (String value) { person.name = value; },
-//                    validator: _validateName,
                   ),
                   const SizedBox(height: 24.0),
-//                  TextFormField(
-//                    decoration: const InputDecoration(
-//                      border: UnderlineInputBorder(),
-//                      filled: true,
-//                      icon: Icon(Icons.phone),
-//                      hintText: 'Where can we reach you?',
-//                      labelText: 'Phone Number *',
-//                      prefixText: '+1',
-//                    ),
-//                    keyboardType: TextInputType.phone,
-//                    onSaved: (String value) { person.phoneNumber = value; },
-//                    validator: _validatePhoneNumber,
-//                    // TextInputFormatters are applied in sequence.
-//                    inputFormatters: <TextInputFormatter> [
-//                      WhitelistingTextInputFormatter.digitsOnly,
-//                      // Fit the validating format.
-//                      _phoneNumberFormatter,
-//                    ],
-//                  ),
-//                  const SizedBox(height: 24.0),
                   TextFormField(
-                    decoration: const InputDecoration(
+                    decoration: InputDecoration(
                       border: UnderlineInputBorder(),
                       filled: true,
                       icon: Icon(Icons.email),
-                      hintText: 'Your email address',
-                      labelText: 'Email *',
+                      labelText: allTranslations.text('words.email') + '*',
                     ),
                     initialValue: _userRepository.currentUser.email,
                     keyboardType: TextInputType.emailAddress,
@@ -310,60 +194,22 @@ class ContactUsState extends State<ContactUs> {
                   ),
                   const SizedBox(height: 24.0),
                   TextFormField(
-                    decoration: const InputDecoration(
+                    decoration: InputDecoration(
                       border: OutlineInputBorder(),
-                      hintText: 'Tell us about your thoughts on our product.',
-                      helperText: 'Tell us about what help that you need.',
-                      labelText: 'Message',
+                      labelText: allTranslations.text('words.message'),
                     ),
                     maxLines: 5,
                     onSaved: (String value) { person.message = value; },
                   ),
-//                  const SizedBox(height: 24.0),
-//                  TextFormField(
-//                    keyboardType: TextInputType.number,
-//                    decoration: const InputDecoration(
-//                      border: OutlineInputBorder(),
-//                      labelText: 'Salary',
-//                      prefixText: '\$',
-//                      suffixText: 'USD',
-//                      suffixStyle: TextStyle(color: Colors.green),
-//                    ),
-//                    maxLines: 1,
-//                  ),
-//                  const SizedBox(height: 24.0),
-//                  PasswordField(
-//                    fieldKey: _passwordFieldKey,
-//                    helperText: 'No more than 8 characters.',
-//                    labelText: 'Password *',
-//                    onFieldSubmitted: (String value) {
-//                      setState(() {
-//                        person.password = value;
-//                      });
-//                    },
-//                  ),
-//                  const SizedBox(height: 24.0),
-//                  TextFormField(
-//                    enabled: person.password != null && person.password.isNotEmpty,
-//                    decoration: const InputDecoration(
-//                      border: UnderlineInputBorder(),
-//                      filled: true,
-//                      labelText: 'Re-type password',
-//                    ),
-//                    maxLength: 8,
-//                    obscureText: true,
-//                    validator: _validatePassword,
-//                  ),
                   const SizedBox(height: 24.0),
                   Center(
                     child: RaisedButton(
-                      child: const Text('SUBMIT'),
+                      child: Text(allTranslations.text('words.submit')),
                       onPressed: _handleSubmitted,
                     ),
                   ),
                   const SizedBox(height: 24.0),
-                  Text(
-                    '* indicates required field',
+                  Text(allTranslations.text('app.contact-screen.form-indication'),
                     style: Theme.of(context).textTheme.caption,
                   ),
                   const SizedBox(height: 24.0),
